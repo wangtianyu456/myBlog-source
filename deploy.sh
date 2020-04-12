9290cfc7373e3e9ba989ae0e96814cc8e7359883
王天宇 <769423688@qq.com>
2020-04-12 16:50:59 +0800

    update: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ cd docs/.vuepress/dist
 # 创建git的本地仓库，提交更改
 git init
 git add .
-git commit -m "add: 新增数组扁平化"
+git commit -m "add: 新增call、apply、bind"
+
+git remote add origin https://github.com/wangtianyu456/MyBlog.git
 
 # 覆盖式的将本地仓库发布至GitHub，因为发布不需要历史记录
 # 格式为 git push -f git@github.com:'用户名'/'仓库名'.git master
-# git push -f git@github.com:wangtianyu456/MyBlog.git master
-git push git@github.com:wangtianyu456/MyBlog.git master
-
+git push -f origin master
 
 cd -
