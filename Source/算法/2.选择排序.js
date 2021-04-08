f3fa01d05996884456d7f33017a5f6ce0ffa0a99
wangtianyu456 <769423688@qq.com>
2021-04-09 00:18:14 +0800

    [add] 选择排序

---
+++ Source/算法/2.选择排序.js
@@ -0,0 +1,16 @@
+function selectSort(arr) {
+  const len = arr.length
+  let minIndex
+  for (let i = 0; i < len; i++) {
+    minIndex = i
+    for (let j = i; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+    if (minIndex !== i) {
+      ;[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+    }
+  }
+  return arr
+}
